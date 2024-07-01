dd6aae60c9881fb10b5be1ec0417aa120645f1f1
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2024-07-01 15:57:00 +0200

    df

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -365,9 +365,8 @@
                             <div class="accordion-body">
                                 <section>
                                     <h3> Posizione </h3>
-                                    @dump($property_posizione['foto_maps'])
                                     @if(isset($property_posizione["foto_maps"]) && $property_posizione['foto_maps'] !== "")
-                                        <img src="{{ asset('public/storage/' . $property_posizione['foto_maps']) }}" alt="{{ $property_posizione['foto_maps'] }}">
+                                        <img class="w-100 rounded" src="{{ asset('public/storage/' . $property_posizione['foto_maps']) }}" alt="{{ $property_posizione['foto_maps'] }}">
                                         <div class="input-group mb-3">
                                             <input type="file" class="form-control" aria-describedby="basic-addon1" name="foto_maps">
                                         </div>
