631cf5c1e2e35c30d9e8648e4596e58cc2811e7c
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2024-08-12 14:06:53 +0200

    as

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- Favicon --}}
-    <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('public/favicon.png') }}" type="image/x-icon">
     <title>Dashboard +</title>
     @vite('resources/js/app.js')
     <style>
