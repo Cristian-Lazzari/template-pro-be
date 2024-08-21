3971d52f36d30e6523ac2ad46e1e0ea1212bd84e
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2024-08-21 17:09:13 +0200

    asd

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -73,7 +73,7 @@
                                 <path d="M3 4.5h10a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2m0 1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1zM1 2a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 2m0 12a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 14"/>
                             </svg>
                         </a>
-                        <a href="{{ route('admin.orders.index') }}" class="plus">
+                        <a href="https://demo3-futureplus.netlify.app/ordina" class="plus">
                             <div class="line"></div>
                             <div class="line l2"></div>
                         </a>
@@ -104,7 +104,7 @@
                                 <path d="M3 4.5h10a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2m0 1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1zM1 2a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 2m0 12a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 14"/>
                             </svg>
                         </a>
-                        <a href="{{ route('admin.reservations.index') }}" class="plus">
+                        <a href="https://demo3-futureplus.netlify.app/check-out" class="plus">
                             <div class="line"></div>
                             <div class="line l2"></div>
                         </a>
