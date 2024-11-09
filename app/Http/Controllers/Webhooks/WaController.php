273da9ea860db818cc7b4be1dc124df4ca19b814
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2024-11-09 17:21:44 +0100

    sd

--- app/Http/Controllers/Webhooks/WaController.php
+++ app/Http/Controllers/Webhooks/WaController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Webhooks;
 
-use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
@@ -24,46 +23,44 @@ class WaController extends Controller
 
     // Metodo per gestire i webhook
     public function handle(Request $request)
-    {
-        $data = $request->all();
-        //Log::warning("Webhook ricevuto", $data);
+{
+    $data = $request->all();
+    Log::warning("Webhook ricevuto", $data);
 
-        // Naviga nella struttura del webhook
-        if (isset($data['entry'][0]['changes'][0]['value']['messages'][0])) {
-            $message = $data['entry'][0]['changes'][0]['value']['messages'][0];
+    // Naviga nella struttura del webhook
+    if (isset($data['entry'][0]['changes'][0]['value']['messages'][0])) {
+        $message = $data['entry'][0]['changes'][0]['value']['messages'][0];
 
-            // Controlla se il messaggio ha un'interazione con pulsante
-            if (isset($message['button']) && isset($message['button']['text'])) {
-                $buttonText = $message['button']['text']; // Testo del pulsante premuto
-                $messageId = $message['id']; // ID del messaggio ricevuto
+        // Controlla se il messaggio ha un'interazione con pulsante
+        if (isset($message['button']) && isset($message['button']['text'])) {
+            $buttonText = $message['button']['text']; // Testo del pulsante premuto
+            $messageId = $message['id']; // ID del messaggio ricevuto
 
-                
-                // Trova l'ordine corrispondente tramite l'ID del messaggio
-                $order = Order::where('whatsapp_message_id', $messageId)->firstOrFail();
-                Log::warning("Pulsante premuto: $buttonText, ID messaggio: $messageId");
-                
-                if ($order) {
-                    if ($buttonText === 'Conferma') {
-                        // Aggiorna lo stato dell'ordine a "Confermato"
-                        $order->status = 5;
-                        Log::warning("Confermato");
-                    } elseif ($buttonText === 'Annulla') {
-                        // Aggiorna lo stato dell'ordine a "Annullato"
-                        $order->status = 0;
-                        Log::warning("Annullato");
-                    }
+            Log::warning("Pulsante premuto: $buttonText, ID messaggio: $messageId");
 
-                    $order->update();
+            // Trova l'ordine corrispondente tramite l'ID del messaggio
+            $order = Order::where('whatsapp_message_id', $messageId)->first();
+
+            if ($order) {
+                if ($buttonText === 'Conferma') {
+                    // Aggiorna lo stato dell'ordine a "Confermato"
+                    $order->status = 5;
+                } elseif ($buttonText === 'Annulla') {
+                    // Aggiorna lo stato dell'ordine a "Annullato"
+                    $order->status = 0;
                 }
-            } else {
-                //Log::warning("Nessun pulsante trovato nel messaggio interattivo.");
+
+                $order->save();
             }
         } else {
-            //Log::warning("Struttura del messaggio non valida o messaggio mancante.");
+            Log::warning("Nessun pulsante trovato nel messaggio interattivo.");
         }
-
-        return response()->json(['status' => 'success']);
+    } else {
+        Log::warning("Struttura del messaggio non valida o messaggio mancante.");
     }
+
+    return response()->json(['status' => 'success']);
+}
 
 
 
