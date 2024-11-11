16c36b650dfdbeff0d90462fc989636ba007ffd8
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2024-11-11 15:31:58 +0100

    sd

--- app/Http/Controllers/Webhooks/WaController.php
+++ app/Http/Controllers/Webhooks/WaController.php
@@ -8,6 +8,7 @@ use App\Models\Date;
 use App\Models\Order;
 use App\Models\Setting;
 use App\Models\Reservation;
+use App\Models\OrderProduct;
 use Illuminate\Http\Request;
 use App\Mail\confermaOrdineAdmin;
 use Illuminate\Support\Facades\Log;
