3a52c0333dc0834b61136245b6168097c914ba9c
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2024-11-30 21:38:34 +0100

    as

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,7 +19,7 @@
                 Esegui l'accesso per vedere i tuoi contenuti!
             </p>
             @if (config('configurazione.APP_URL') == 'https://db-demo4.future-plus.it')
-                <p>(Le credenziali per accedere alla demo sono email: <strong>demo@demo.it</strong> password: <strong>demo1</strong>) </p>
+                <p style="font-style: italic">(Le credenziali per accedere alla demo sono email: <strong>demo@demo.it</strong> password: <strong>demo1</strong>) </p>
             @endif
         </div>
     </header>
