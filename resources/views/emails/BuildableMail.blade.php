203603f482101243c26ebc65d77ff0c7c2dbe407
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2025-01-27 14:57:18 +0100

    sd

--- resources/views/emails/BuildableMail.blade.php
+++ resources/views/emails/BuildableMail.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Conferma Email</title>
 </head>
-<body style="font-family: Arial, sans-serif; background-color: #e9f0fb; color: #161c3e; margin: 0; padding: 10px 0px; width: 100%;">
+<body style="font-family: Arial, sans-serif; background-color: #e9f0fb; color: #161c3e; margin: 0; padding: 10px 0px 0px; width: 100%;">
    <!-- Footer -->
    @php
        $locale = 'it_IT';
@@ -21,13 +21,13 @@
             IntlDateFormatter::NONE  // Nessun orario
         );
    @endphp
-    <img style="width: 80px; margin: 2rem;" src="{{config('configurazione.APP_URL') . '/public/favicon.png'}}" alt="">
+    <img style="width: 80px; margin: 2rem; filter: drop-shadow( 0 0 15px rgba(5, 1, 31, 0.496)) " src="{{config('configurazione.APP_URL') . '/public/favicon.png'}}" alt="">
 
 
     <h1 style="color: #04001d; font-size: 28px; padding: 20px;">{{$content_mail['heading']}}</h1>
     
     @if($content_mail['img_1'] !== NULL)   
-    <img style="width: 40%; margin-top: 2rem; margin-bottom: 2rem; margin-left: 30%;" src="{{config('configurazione.APP_URL') . '/public/storage/' . $content_mail['img_1']}}" alt="">
+    <img style="border-radius: 10px; width: 40%; margin-top: 2rem; margin-bottom: 2rem; margin-left: 30%;" src="{{config('configurazione.APP_URL') . '/public/storage/' . $content_mail['img_1']}}" alt="">
     @endif
     
     <div style="margin: 1rem 3rem; font-size: 20px;" class="corpo">
@@ -37,7 +37,7 @@
     </div>
     
     @if($content_mail['img_2'] !== NULL)   
-        <img style="width: 20%; margin-top: 2rem; margin-bottom: 2rem; margin-left: 40%;" src="{{config('configurazione.APP_URL') . '/public/storage/' . $content_mail['img_2']}}" alt="">
+        <img style="border-radius: 10px; width: 20%; margin-top: 2rem; margin-bottom: 2rem; margin-left: 40%;" src="{{config('configurazione.APP_URL') . '/public/storage/' . $content_mail['img_2']}}" alt="">
     @endif
 
     <p style="color: #04001d; font-size: 24px; text-align: center; margin: .6rem 0">{{$content_mail['ending']}}</p>
@@ -48,22 +48,18 @@
     </div>
 
     <footer style=" margin: 50px 0 0; background-color: black; color: white; padding: 10px 20px; text-align: center; font-size: 12px;">
-        <h5 style="font-size: 16px">Seguici sui social</h5>
+        <h5 style="font-size: 16px margin: 0;">Seguici sui social</h5>
         <div style="padding: 0 0 20px "> 
             <a style="color: white; text-decoration: none; margin: 0 auto;" href="https://www.facebook.com/profile.php?id=61558817374447">
                 Facebook: <span style="color: white; font-weight: 900"> Future plus</span>
             </a>
-            <a style="color: white; text-decoration: none; margin: auto;" href="https://www.instagram.com/future.plus_/?hl=it">
+            <a style="color: white; text-decoration: none; margin: 0 auto;" href="https://www.instagram.com/future.plus_/?hl=it">
                 Instagram: <span style="color: white; font-weight: 900"> @future.plus_</span>
             </a>
         </div>
-        <p style="font-size: 12px; font-family: monospace; line-height: 1.5; margin: 15px 10px 5px;">&copy; 2024 {{ config('configurazione.APP_NAME') }}. Tutti i diritti riservati.</p>
-        <p style="font-size: 17px; line-height: 1.5; margin: 5px;" > Powered by <a style="color: white; text-decoration: none" href="https://future-plus.it">Future +</a></p>
+        <p style="font-size: 12px; font-family: monospace; line-height: 1.5; margin: 10px 5px;">&copy; 2024 {{ config('configurazione.APP_NAME') }}. Tutti i diritti riservati.</p>
+        <p style="font-size: 15px; line-height: 1.5; margin: 5px;" > Powered by <a style="color: white; text-decoration: none; font-weight:900;" href="https://future-plus.it">Future +</a></p>
     </footer>
 </body>
 </html>
-<style>
-    div{
-        justify-content: flex-start;
-    }
-</style>
+
