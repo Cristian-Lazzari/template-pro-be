456ef44869099d9ee4f718fe13b3335386f289bc
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2025-02-03 21:34:15 +0100

    elimino tutto

--- resources/views/guests/home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.guest')
-
-@section('contents')
-    <div class="container my-3">
-        <a class="my_btn_2" href="{{route('login')}}">Accedi</a>
-    </div>
-@endsection
-    {{-- <a href="{{route('admin.dashboard')}}">indexadmin</a> --}}
