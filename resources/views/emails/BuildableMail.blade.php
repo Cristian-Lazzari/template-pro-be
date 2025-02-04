79cb0e582b4bf1682f41859d4dcc8fe7c0ea46f7
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2025-02-04 17:43:24 +0100

    asd

--- resources/views/emails/BuildableMail.blade.php
+++ resources/views/emails/BuildableMail.blade.php
@@ -21,7 +21,7 @@
             IntlDateFormatter::NONE  // Nessun orario
         );
    @endphp
-    <img style="width: 80px; margin: 2rem; background-color: #090333; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.272); padding: 5px; border: solid 2px #090333;" src="{{config('configurazione.APP_URL') . '/public/favicon.png'}}" alt="">
+    <img style="width: 80px; margin: 25px; background-color: #090333; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.272); padding: 5px; border: solid 2px #090333;" src="{{config('configurazione.APP_URL') . '/public/favicon.png'}}" alt="">
 
 
     <h1 style="color: #04001d; font-size: 28px; padding: 20px;">{{$content_mail['heading']}}</h1>
@@ -30,19 +30,19 @@
     <img style="border-radius: 10px; width: 40%; margin-top: 2rem; margin-bottom: 2rem; margin-left: 30%;" src="{{config('configurazione.APP_URL') . '/public/storage/' . $content_mail['img_1']}}" alt="">
     @endif
     
-    <div style="margin: 3rem; font-size: 20px; color: rgb(28, 28, 29)" class="corpo">
+    <div style="margin: 30px 25px; font-size: 20px; color: rgb(28, 28, 29)" class="corpo">
         @foreach ($content_mail['body'] as $b)
         <p>{{$b}}</p>
         @endforeach
     </div>
     
     @if($content_mail['img_2'] !== NULL)   
-        <img style="border-radius: 10px; width: 20%; margin-top: 2rem; margin-bottom: 2rem; margin-left: 40%;" src="{{config('configurazione.APP_URL') . '/public/storage/' . $content_mail['img_2']}}" alt="">
+        <img style="border-radius: 10px; width: 60%; margin-top: 2rem; margin-bottom: 2rem; margin-left: 20%;" src="{{config('configurazione.APP_URL') . '/public/storage/' . $content_mail['img_2']}}" alt="">
     @endif
 
     <p style="color: #04001d; font-size: 22px; text-align: center; margin: .6rem 30px">{{$content_mail['ending']}}</p>
 
-    <div class="sender" style="color: #04001d">
+    <div class="sender" style="color: #04001d; margin: 50px 0">
         <p style="font-weight: 900; font-size: 18px; margin: 1rem 2rem 0">{{$content_mail['sender']}}</p>
         <p style="font-style: italic; font-size: 16px; margin: 4px 2rem 1rem" class="date">{{$formatter->format($dataOdierna)}}</p>
     </div>
