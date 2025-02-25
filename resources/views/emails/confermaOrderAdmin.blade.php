e9970cc86198c54672e8ccb7ab4424a76f2c7c76
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2025-02-25 01:27:30 +0100

    asd

--- resources/views/emails/confermaOrderAdmin.blade.php
+++ resources/views/emails/confermaOrderAdmin.blade.php
@@ -133,8 +133,8 @@
             @endif
         @endif
 
-        @if (isset($content_mail['whatsapp_message_id']) && config('configurazione.subscription') == 3 && $content_mail['to'] == 'user' && !in_array($content_mail['status'], [0, 6]))
-        <p style="line-height: 1.5; margin: 15px;" >Per annullare l'ordine o la prenotazione premi questo bottone </p>
+        @if (isset($content_mail['whatsapp_message_id']) && config('configurazione.subscription') == 3 && $content_mail['to'] == 'user')
+            <p style="line-height: 1.5; margin: 15px;" >Per annullare l'ordine o la prenotazione premi questo bottone </p>
             <p style="line-height: 1.5; margin: 15px;">
                 <a href="{{config('configurazione.APP_URL')}}/api/client_default/?whatsapp_message_id={{$content_mail['whatsapp_message_id']}}" style="background-color: #9f2323f0; color: rgb(255, 255, 255); padding: 8px 17px; text-align: center; text-decoration: none; border-radius: 35px; font-size: 15px;">Annulla</a>
             </p>
