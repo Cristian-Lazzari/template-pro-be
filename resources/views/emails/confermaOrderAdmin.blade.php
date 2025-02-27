2b9d3681e0d8ee6a11e40e35c97cfd88800c2f82
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2025-02-27 20:58:13 +0100

    cfg

--- resources/views/emails/confermaOrderAdmin.blade.php
+++ resources/views/emails/confermaOrderAdmin.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Conferma Email</title>
 </head>
 <body style="font-family: Arial, sans-serif; background-color: #f4f4f4; color: #04001d; margin: 0; padding: 0;">
     <div style="max-width: 600px; margin: 10px auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);">
@@ -20,7 +19,7 @@
         </center>
 
 
-        <h1 style="color: #04001d; font-size: 24px; text-align: center;">{{$content_mail['title']}}</h1>
+        <h1 style="text-transform :uppercase; color: #04001d; font-size: 24px;">{{$content_mail['title']}}</h1>
         @if (isset($content_mail['subtitle']))
         <h4 style="color: #04001db9; font-size: 16px; ">{{$content_mail['subtitle']}}</h4>
         @endif
