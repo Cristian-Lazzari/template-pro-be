e68713df24dc054fa6f26db499dbb4a12fd8af6d
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2025-02-28 16:01:59 +0100

    asd

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -28,7 +28,7 @@ class ProductController extends Controller
             $query = $query->where('category_id', $categoryId);
         } 
         
-        $products = $query->where('archived', 0)->orderBy('created_at')->get();
+        $products = $query->where('archived', 0)->orderBy('created_at', 'asc')->get();
         
         return response()->json([
             'success'   => true,
