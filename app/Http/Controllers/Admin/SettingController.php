3aab46e68e72fc4a87a558b1486619ff7a160698
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2025-05-01 20:50:38 +0200

    zxc

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -72,7 +72,7 @@ class SettingController extends Controller
             }
 
             $property['too'] = $request->too;
-            $property['dt'] = $request->dt;
+            $property['dt'] = $request->services == 3 ? $request->dt :  0;
             $property['services'] = $request->services;
             $property['menu_fix_set'] = $request->menu_fix_set;
             
