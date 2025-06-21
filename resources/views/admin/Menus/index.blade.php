2cec139577d7faa2d742e1742f4e9e06a30b96ed
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2025-06-21 12:13:05 +0200

    sd

--- resources/views/admin/Menus/index.blade.php
+++ resources/views/admin/Menus/index.blade.php
@@ -8,7 +8,7 @@
     @php
         $data = session('success')
     @endphp
-   <div class="alert alert-primary alert-dismissible fade show" role="alert">
+   <div class="alert alert-info alert-dismissible fade show" role="alert">
     {{ $data }} 
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
