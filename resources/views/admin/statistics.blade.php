a5f35f6c216f636e9bedc805c6e26eab0b138d9f
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2025-11-05 16:44:25 +0100

    asd

--- resources/views/admin/statistics.blade.php
+++ resources/views/admin/statistics.blade.php
@@ -175,7 +175,7 @@
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
                                 </svg>
                             </span>
-                            <div class="donut-wrapper" style="--percent: {{ ($res_people['adults_confirmed'] + ($res_people['children_confirmed'] )/ $res_count * 100}}">
+                            <div class="donut-wrapper" style="--percent: {{ ($res_people['adults_confirmed'] + $res_people['children_confirmed'] ) / $res_count * 100}}">
                                 <p>
                                     {{ $res_people['adults_confirmed'] }}
                                 </p>
