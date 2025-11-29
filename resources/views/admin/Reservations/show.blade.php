52bdc169b5354438b29f09f646607ebacb555a16
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2025-11-29 13:52:36 +0100

    sd

--- resources/views/admin/Reservations/show.blade.php
+++ resources/views/admin/Reservations/show.blade.php
@@ -69,7 +69,7 @@
                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                             <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
                         </svg>
-                        {{$reservation->status == 5 ? 'Confermata' : 'Confermatra e Incassata'}}
+                        {{$reservation->status == 1 ? 'Confermata' : 'Confermatra e Incassata'}}
                     </div>
                 @endif
             </div>
@@ -118,10 +118,10 @@
                     Marketing sul contatto: {{$reservation->news_letter ? 'si' : 'no'}}
                 </div>
                 <div class="actions">
-                    @if (!in_array($reservation->status, [0, 1, 5]))
+                    @if (in_array($reservation->status, [0, 2, 3]))
                         <button type="button" data-bs-toggle="modal" data-bs-target="#confirmModal" class=" my_btn_3">Conferma</button>
                     @endif
-                    @if(!in_array($reservation->status, [0, 1]))
+                    @if(in_array($reservation->status, [1, 2, 3, 5]))
                         <button type="button" data-bs-toggle="modal" data-bs-target="#cancelModal" class=" my_btn_5">{{$reservation->status == 5 ? 'Rimborsa e Annulla' : 'Annulla'}}</button>                   
 
                     @endif
@@ -134,9 +134,9 @@
     </div>
 
       {{-- Modale per conferma --}}
-      <div class="modal fade" id="confirmModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
+    <div class="modal fade" id="confirmModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content mymodal_make_res">
                 <div class="modal-header c-1">
                     <h1 class="modal-title fs-5" id="confirmModalLabel">Gestione notifica conferma</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -168,35 +168,127 @@
 
     {{-- Modale per annullamento --}}
     <div class="modal fade" id="cancelModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="cancelModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header c-1">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content mymodal_make_res">
+                <div class="modal-header">
                     <h1 class="modal-title fs-5" id="cancelModalLabel">Gestione notifica annullamento</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body c-1">
+                <div class="modal-body">
                     Ordine di: {{$reservation->name}} 
                     per il: {{$reservation->date_slot}}
                     <p>Vuoi inviare un messaggio whatsapp?</p>
-                </div>
-                <div class="modal-footer">
-                    <form action="{{ route('admin.reservations.status') }}" method="POST">
-                        @csrf
-                        <input value="1" type="hidden" name="wa">
-                        <input value="0" type="hidden" name="c_a">
-                        <input value="{{$reservation->id}}" type="hidden" name="id">
-                        <button type="submit" class="w-100 my_btn_6">Si</button>
-                    </form>
-                    <form action="{{ route('admin.reservations.status') }}" method="POST">
-                        @csrf
-                        <input value="0" type="hidden" name="wa">
-                        <input value="0" type="hidden" name="c_a">
-                        <input value="{{$reservation->id}}" type="hidden" name="id">
-                        <button type="submit" class="w-100 my_btn_6">NO</button>
-                    </form>
+                    <div class="actions">
+                        <form action="{{ route('admin.reservations.status') }}" method="POST">
+                            @csrf
+                            <input value="0" type="hidden" name="wa">
+                            <input value="0" type="hidden" name="c_a">
+                            <input value="{{$reservation->id}}" type="hidden" name="id">
+                            <button type="submit" class="w-100 my_btn_2">NO</button>
+                        </form>
+                        <form action="{{ route('admin.reservations.status') }}" method="POST">
+                            @csrf
+                            <input value="1" type="hidden" name="wa">
+                            <input value="0" type="hidden" name="c_a">
+                            <input value="{{$reservation->id}}" type="hidden" name="id">
+                            <button type="submit" class="w-100 my_btn_3">SI</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+{{-- 
+    <div class="modal fade" id="exampleModal{{$item->id}}" tabindex="-1" aria-labelledby="exampleModal{{$item->id}}Label" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <button type="button" class="btn_close" data-bs-dismiss="modal">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+                        </svg>
+                        Chiudi
+                    </button>
+                    <div class="action_top">
+                        <a href="{{ route('admin.posts.edit', $item) }}" class="edit">
+                            <svg style="vertical-align: sub" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                            </svg>
+                        </a>
+                        
+                        <form action="{{ route('admin.posts.status') }}" method="POST">
+                            @csrf
+                            <input type="hidden" name="archive" value="0">
+                            <input type="hidden" name="v" value="1">
+                            <input type="hidden" name="a" value="0">
+                            <input type="hidden" name="id" value="{{$item->id}}">
+                            <button type="submit" class=" edit
+                                @if(!$item->visible) not @endif 
+                                visible">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
+                                    <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-slash-fill" viewBox="0 0 16 16">
+                                    <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z"/>
+                                    <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z"/>
+                                </svg>
+                            </button>
+                        </form>
+                        <form action="{{ route('admin.posts.status') }}" method="POST">
+                            @csrf
+                            <input type="hidden" name="archive" value="0">
+                            <input type="hidden" name="v" value="0">
+                            <input type="hidden" name="a" value="1">
+                            <input type="hidden" name="id" value="{{$item->id}}">
+                            <button class="edit" type="submit">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+                                </svg>
+                            </button>
+                        </form>
+                
+                    </div>
+                    <div class="name_cat">
+                        <div class="name">{{$item->title}}</div>
+                        <div class="cat">{{$item->path}}</div>
+                    </div>
+                    @if ($item->description)
+                        <section>
+                            <h4>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-card-text" viewBox="0 0 16 16">
+                                    <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
+                                    <path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8m0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5"/>
+                                </svg>
+                                Descrizione</h4>
+                            <p>{{$item->description}}</p>
+                        </section>
+                    @endif
+                    @if ($item->hashtag)
+                        <section>
+                            <h4>
+                                <strong>#</strong>
+                                Hashtags</h4>
+                            <p>{{$item->hashtag}}</p>
+                        </section>
+                    @endif
+                    @if ($item->link)
+                        <section>
+                            <h4>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16">
+                                    <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+                                    <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+                                </svg>
+                                Link</h4>
+                            <a href="{{$item->link}}">{{$item->link}}</a>
+                        </section>
+                    @endif
+
+
+                </div>
+            </div>
+        </div>
+    </div> --}}
 
 @endsection
