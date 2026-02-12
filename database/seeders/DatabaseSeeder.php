e28c1f6bb027bc3c0bcbd9c6e9248f80d534783a
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2026-02-12 15:38:48 +0100

    asd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
             
             //CategoriesTableSeeder::class,
             ProductsTableSeeder::class,
-            PostsTableSeeder::class,
+            //PostsTableSeeder::class,
             SettingsTableSeeder::class,
         ]);
     }
