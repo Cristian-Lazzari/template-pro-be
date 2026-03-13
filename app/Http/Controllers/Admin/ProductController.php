45d295059a28f83f82e911d224c77f48323d9786
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2026-03-13 21:43:49 +0100

    sd

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -174,8 +174,7 @@ class ProductController extends Controller
             unset( $data['image_ing']);
             return to_route('admin.products.create')->with('ingredient_success', $data);     
         }
-
-       
+  
         $request->validate($this->validationsFalse);
         
         $product = new Product();
@@ -189,6 +188,8 @@ class ProductController extends Controller
         $product->category_id   = $data['category_id'];
 
         $product->price         = $price * 100;       
+        $product->name   = isset($data['name']);
+        $product->description   = isset($data['description']);
         $product->promotion   = isset($data['promotion']) ? true : false;
         $product->old_price   = isset($data['old_price']) ? (float) str_replace(',', '.', $data['old_price']) * 100 : null;
         
