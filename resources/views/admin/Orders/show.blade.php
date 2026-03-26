4b819c9724fa39ef7c870d3031ee68958f97004c
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2026-03-26 14:41:04 +0100

    asd

--- resources/views/admin/Orders/show.blade.php
+++ resources/views/admin/Orders/show.blade.php
@@ -52,28 +52,7 @@
                 my_6
             @endif myres"
         >
-
-            <div class="mail-tel">
-                <a href="{{'mailto:' . $order->email}}" class="mail">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-arrow-up-fill" viewBox="0 0 16 16">
-                        <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zm.192 8.159 6.57-4.027L8 9.586l1.239-.757.367.225A4.49 4.49 0 0 0 8 12.5c0 .526.09 1.03.256 1.5H2a2 2 0 0 1-1.808-1.144M16 4.697v4.974A4.5 4.5 0 0 0 12.5 8a4.5 4.5 0 0 0-1.965.45l-.338-.207z"/>
-                        <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.354-5.354 1.25 1.25a.5.5 0 0 1-.708.708L13 12.207V14a.5.5 0 0 1-1 0v-1.717l-.28.305a.5.5 0 0 1-.737-.676l1.149-1.25a.5.5 0 0 1 .722-.016"/>
-                    </svg>
-                    <span>
-                        {{$order->email}}
-                    </span>
-                </a>
-                <a href="{{'tel:' . $order->phone}}" class="tel">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-outbound-fill" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877zM11 .5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-4.146 4.147a.5.5 0 0 1-.708-.708L14.293 1H11.5a.5.5 0 0 1-.5-.5"/>
-                    </svg>
-                    <span>
-
-                        {{$order->phone}}
-                    </span>
-                </a>
-            </div>
-            <div class="status">
+            <div class="status mb-4">
                 @if(in_array($order->status, [0, 6])) 
                     <div class="int null">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi null bi-x-circle" viewBox="0 0 16 16">
@@ -101,6 +80,27 @@
                     </div>
                 @endif
             </div>
+            <div class="mail-tel">
+                <a href="{{'mailto:' . $order->email}}" class="mail">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-arrow-up-fill" viewBox="0 0 16 16">
+                        <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zm.192 8.159 6.57-4.027L8 9.586l1.239-.757.367.225A4.49 4.49 0 0 0 8 12.5c0 .526.09 1.03.256 1.5H2a2 2 0 0 1-1.808-1.144M16 4.697v4.974A4.5 4.5 0 0 0 12.5 8a4.5 4.5 0 0 0-1.965.45l-.338-.207z"/>
+                        <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.354-5.354 1.25 1.25a.5.5 0 0 1-.708.708L13 12.207V14a.5.5 0 0 1-1 0v-1.717l-.28.305a.5.5 0 0 1-.737-.676l1.149-1.25a.5.5 0 0 1 .722-.016"/>
+                    </svg>
+                    <span>
+                        {{$order->email}}
+                    </span>
+                </a>
+                <a href="{{'tel:' . $order->phone}}" class="tel">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-outbound-fill" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877zM11 .5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-4.146 4.147a.5.5 0 0 1-.708-.708L14.293 1H11.5a.5.5 0 0 1-.5-.5"/>
+                    </svg>
+                    <span>
+
+                        {{$order->phone}}
+                    </span>
+                </a>
+            </div>
+            
             <div class="body">
                 <section class="first_section">
                     <div class="data_cont">
@@ -284,8 +284,11 @@
                 <h1 class="modal-title fs-3" id="confirmModalLabel">{{ __('admin.Gestione_notifica_per_conferma') }}</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body fs-4">{{ __('admin.Ordine_di') }}<strong>{{$order->name}} </strong>{{ __('admin.per_il') }}<strong>{{$order->date_slot}}</strong>
-                <p>{{ __('admin.Oltre_alla_mail_automatica_vuoi_anche_inviare_un_messaggio_su_whatsapp') }}</p>
+            <div class="modal-body fs-4">
+                <p>
+                    {{ __('admin.Ordine_di') }} <strong>{{$order->name}} </strong>{{ __('admin.per_il') }} <strong>{{$order->date_slot}}</strong>
+                </p>
+                {{-- <p>{{ __('admin.Oltre_alla_mail_automatica_vuoi_anche_inviare_un_messaggio_su_whatsapp') }}</p> --}}
             </div>
             <div class="modal-footer">
                 <form action="{{ route('admin.orders.status') }}" method="POST">
@@ -293,15 +296,15 @@
                     <input value="0" type="hidden" name="wa">
                     <input value="1" type="hidden" name="c_a">
                     <input value="{{$order->id}}" type="hidden" name="id">
-                    <button type="submit" class="w-100 my_btn_2">No</button>
+                    <button type="submit" class="w-100 my_btn_2">{{__('admin.Conferma')}}</button>
             </form>
-            <form action="{{ route('admin.orders.status') }}" method="POST">
+            {{-- <form action="{{ route('admin.orders.status') }}" method="POST">
                     @csrf
                     <input value="1" type="hidden" name="wa">
                     <input value="1" type="hidden" name="c_a">
                     <input value="{{$order->id}}" type="hidden" name="id">
                     <button type="submit" class="w-100 my_btn_3">Si</button>
-            </form>
+            </form> --}}
             </div>
         </div>
     </div>
@@ -315,8 +318,11 @@
                 <h1 class="modal-title fs-3" id="cancelModalLabel">{{ __('admin.Gestione_notifica_per_annullamento') }}</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body fs-4">{{ __('admin.Ordine_di') }}<strong>{{$order->name}} </strong>{{ __('admin.per_il') }}<strong>{{$order->date_slot}}</strong>
-                <p>{{ __('admin.Oltre_alla_mail_automatica_vuoi_anche_inviare_un_messaggio_su_whatsapp') }}</p>
+            <div class="modal-body fs-4">
+                <p>
+                    {{ __('admin.Ordine_di') }} <strong>{{$order->name}} </strong>{{ __('admin.per_il') }} <strong>{{$order->date_slot}}</strong>
+                </p>
+                {{-- <p>{{ __('admin.Oltre_alla_mail_automatica_vuoi_anche_inviare_un_messaggio_su_whatsapp') }}</p> --}}
             </div>
             <div class="modal-footer">
                 <form action="{{ route('admin.orders.status') }}" method="POST">
@@ -324,15 +330,15 @@
                     <input value="0" type="hidden" name="wa">
                     <input value="0" type="hidden" name="c_a">
                     <input value="{{$order->id}}" type="hidden" name="id">
-                    <button type="submit" class="w-100 my_btn_2">No</button>
+                    <button type="submit" class="w-100 my_btn_2">{{__('admin.Annulla')}}</button>
             </form>
-            <form action="{{ route('admin.orders.status') }}" method="POST">
+            {{-- <form action="{{ route('admin.orders.status') }}" method="POST">
                     @csrf
                     <input value="1" type="hidden" name="wa">
                     <input value="0" type="hidden" name="c_a">
                     <input value="{{$order->id}}" type="hidden" name="id">
                     <button type="submit" class="w-100 my_btn_3">Si</button>
-            </form>
+            </form> --}}
             </div>
         </div>
     </div>
